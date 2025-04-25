c3a03a0f7dc76eb289cd1fdd34fb95ec649c58f2
danielhav12 <75470239+danielhav12@users.noreply.github.com>
2025-04-25 19:24:15 +0000

    rozřazeno do složek a změněny odkaty

--- js_houby.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('mobile-nav');
-
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
